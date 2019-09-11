c7c02e1e101a4613eb33ea24f28af480498de48d
Terry.liu <30676507+TerryShare@users.noreply.github.com>
2019-09-11 23:40:51 +0800

    优化脚本

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ cp cmd_track.conf /etc/rsyslog.d/
 cp cmd_profile.sh /etc/profile.d/
 source /etc/profile
 service rsyslog restart
-yum install -y source-highlight
-yum install -y psmisc
+yum install -y  source-highlight
+yum install -y  psmisc
 
